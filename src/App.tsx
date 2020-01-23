7eb5394304674a1a966a6255105b1853bc9221bf
Damaschin Mihai <mihai.damaschin@ebs-integrator.com>
2020-01-23 10:29:31 +0200

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
   return (
@@ -8,7 +7,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsasdx</code> and save to reload!
         </p>
         <a
           className="App-link"
